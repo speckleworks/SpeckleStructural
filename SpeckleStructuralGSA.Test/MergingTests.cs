af543cf96285e60f626d4ae60c202c4855e19823
Nicolaas Burgers <nic.burgers@arup.com>
2019-10-21 15:26:51 +1100

    Added programmatic mappings for AutoMapper for all SpeckleObject types that have ToNative extension methods.

--- SpeckleStructuralGSA.Test/MergingTests.cs
+++ SpeckleStructuralGSA.Test/MergingTests.cs
@@ -4,27 +4,181 @@ using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
 using NUnit.Framework;
-//using AutoMapper;
 using SpeckleGSAInterfaces;
 using Moq;
 using SpeckleStructuralClasses;
 using SpeckleCore;
-using Omu.ValueInjecter;
 using System.Reflection;
-using Omu.ValueInjecter.Injections;
 using AutoMapper;
 
 namespace SpeckleStructuralGSA.Test
 {
+  [TestFixture]
+  public class MergingTests
+  {
+    [Test]
+    public void MergeTestAutomapper()
+    {
+      var newObj = new mTest { d = 5, e1 = eTest.NotSet, e2 = eTest.AVal };
+      var existingStiffness = new StructuralVectorSix(new double[] { 10, 20, 30, 40, 50, 60 });
+      var existingObj = new mTest { s = "Test", e1 = eTest.BVal, dArr = new double[] { 1, 2, 3 }, v = existingStiffness };
+
+      var config = new MapperConfiguration(cfg =>
+      {        
+        cfg.CreateMap<mTest, mTest>();
+        //cfg.CreateMap<StructuralVectorSix, StructuralVectorSix>(); This causes issues
+        cfg.ForAllPropertyMaps(pm => true, (pm, c) => c.ResolveUsing(new IgnoreNullResolver(), pm.SourceMember.Name));
+      });
+
+      config.AssertConfigurationIsValid();
+
+      var m = config.CreateMapper();
+
+      var testResult = m.Map(newObj, existingObj);
+
+      Assert.AreEqual(5, testResult.d);
+      Assert.AreEqual(eTest.BVal, testResult.e1);
+      Assert.AreEqual(eTest.AVal, testResult.e2);
+      Assert.AreEqual("Test", testResult.s);
+    }
+
+    [Test]
+    public void MergeTestGSA()
+    {
+      var ls1 = new object[] { "PROP_SPR.3:{speckle_app_id:gh/a}", 1, "LSPxGeneral", "NO_RGB", "GLOBAL", "GENERAL", 0, 16, 0, 17, 0, 18, 0, 19, 0, 20, 0, 21, 0.21 };
+      var gwa1 = string.Join("\t", ls1.Select(l => l.ToString()));
+
+      PrepareInterfacerForGwaToSpeckle<GSASpringProperty>(gwa1, "PROP_SPR.3", "gh/a");
+
+      //Call the ToSpeckle method, which just adds to the GSASenderObjects collection
+      Conversions.ToSpeckle(new GSASpringProperty());
+      var existing = (StructuralSpringProperty) ((IGSASpeckleContainer)Initialiser.GSASenderObjects[typeof(GSASpringProperty)].First()).Value;
+
+      var newToMerge = new StructuralSpringProperty() { DampingRatio = 1.5 };
+
+      //Find all structural types which have a ToNative static method
+      var assemblies = AppDomain.CurrentDomain.GetAssemblies().Where(a => a.GetTypes().Any(t => typeof(ISpeckleInitializer).IsAssignableFrom(t))).ToList();
+      var speckleTypes = new List<Type>();
+      foreach (var assembly in assemblies)
+      {
+        var types = assembly.GetTypes();
+        foreach (var t in types)
+        {
+          if (typeof(SpeckleObject).IsAssignableFrom(t))
+          {
+            speckleTypes.Add(t);
+          }
+        }
+      }
+
+      var mappableTypes = new List<Type>();
+      foreach (var assembly in assemblies.Where(a => a.FullName.Contains("GSA")))
+      {
+        foreach (var t in speckleTypes)
+        {
+          var methods = GetExtensionMethods(assembly, t, "ToNative");
+          if (methods != null && methods.Count() > 0)
+          {
+            mappableTypes.Add(t);
+          }
+        }
+      }
+
+      var config = new MapperConfiguration(cfg =>
+      {
+        foreach (var t in mappableTypes)
+        {
+          cfg.CreateMap(t, t);
+        }
+        cfg.ForAllPropertyMaps(pm => true, (pm, c) => c.ResolveUsing(new IgnoreNullResolver(), pm.SourceMember.Name));
+        cfg.AddGlobalIgnore(nameof(SpeckleObject.Properties));
+      });
+
+      config.AssertConfigurationIsValid();
+
+      var m = config.CreateMapper();
+
+      var resultingObject = m.Map(newToMerge, existing);
+    }
+
+    private static IEnumerable<MethodInfo> GetExtensionMethods(Assembly assembly, Type extendedType, string methodName)
+    {
+      var methodNameFormatted = methodName.ToLower();
+      var returnMethodInfos = new List<MethodInfo>();
+      var types = assembly.GetTypes();
+      var typesFiltered = types.Where(t => t.IsSealed && !t.IsGenericType && !t.IsNested);
+      foreach (var type in typesFiltered)
+      {
+        var methods = type.GetMethods(BindingFlags.Static | BindingFlags.Public | BindingFlags.NonPublic);
+        var methodsFiltered = methods.Where(m => m.IsDefined(typeof(System.Runtime.CompilerServices.ExtensionAttribute), false));
+
+        foreach (var method in methodsFiltered)
+        {
+          var methodParameters = method.GetParameters();
+          if (methodParameters != null && methodParameters.Count() > 0)
+          if (methodParameters[0].ParameterType == extendedType)
+          {
+            if (string.Equals(method.Name, methodName, StringComparison.InvariantCultureIgnoreCase))
+            {
+              returnMethodInfos.Add(method);
+            }
+          }
+        }
+      }
+
+      return returnMethodInfos;
+      
+      var query = from type in assembly.GetTypes()
+                  where type.IsSealed && !type.IsGenericType && !type.IsNested
+                  from method in type.GetMethods(BindingFlags.Static
+                    | BindingFlags.Public | BindingFlags.NonPublic)
+                  where method.IsDefined(typeof(System.Runtime.CompilerServices.ExtensionAttribute), false)
+                  where method.GetParameters()[0].ParameterType == extendedType
+                  where method.Name == methodName
+                  select method;
+      return query;
+    }
+
+    private void PrepareInterfacerForGwaToSpeckle<T>(string gwaCommand, string keyword, string sid)
+    {
+      var testType = typeof(T);
+
+      var mockGsaInterfacer = new Mock<IGSAInterfacer>();
+      mockGsaInterfacer.Setup(x => x.GetGWARecords(It.IsAny<string>())).Returns((string x) =>
+          {
+            if (x.ToLower().StartsWith("get_all\tprop_spr"))
+            {
+              return new[] { gwaCommand };
+            }
+            return new[] { "" };
+          }
+        );
+      mockGsaInterfacer.Setup(x => x.GetSID(It.IsAny<string>(), It.IsAny<int>())).Returns((string x, int y) =>
+          {
+            var xLower = x.ToLower();
+            if (xLower.StartsWith("prop_spr"))
+            {
+              if (y == 1) return "gh/a";
+            }
+            return "";
+          }
+        );
+      mockGsaInterfacer.Setup(x => x.GetDeletedGWARecords(It.IsAny<string>())).Returns((string x) => { return new[] { "" }; });
+
+      Initialiser.GSASenderObjects = new Dictionary<Type, List<object>>
+      {
+        { testType, new List<object>() }
+      };
+      Initialiser.Interface = mockGsaInterfacer.Object;
+    }
+  }
+
   public enum eTest
   {
     NotSet,
     AVal,
     BVal
   }
-
-  [TestFixture]
-
   public class mTest
   {
     private double _secret;
@@ -35,9 +189,13 @@ namespace SpeckleStructuralGSA.Test
     public eTest e1 { get; set; }
 
     public eTest e2 { get; set; }
+
+    public double[] dArr { get; set; }
+
+    public StructuralVectorSix v { get; set; }
   }
 
-  class IgnoreNullResolver : IMemberValueResolver<object, object, object, object>
+  public class IgnoreNullResolver : IMemberValueResolver<object, object, object, object>
   {
     public object Resolve(object source, object destination, object sourceMember, object destinationMember, ResolutionContext context)
     {
@@ -60,232 +218,4 @@ namespace SpeckleStructuralGSA.Test
       }
     }
   }
-
-  public class NoNullsInjection : LoopInjection
-  {
-    protected override void SetValue(object source, object target, PropertyInfo sp, PropertyInfo tp)
-    {
-      if (sp.GetValue(source) == null) return;
-      base.SetValue(source, target, sp, tp);
-    }
-  }
-
-  public class MergingTests
-  {
-    [Test]
-    public void MergeTestInjecter()
-    {
-      var md = new mTest { d = 5 };
-      var ms = new mTest { s = "Test" };
-
-      var endObj = ms.InjectFrom<NoNullsInjection>(md);
-
-      /*
-      var config = new MapperConfiguration(cfg =>
-      {
-        cfg.CreateMap<mTest, mTest>()
-            .ForAllMembers(opt => opt.Condition((d, b) => d != null));
-      });
-
-      config.AssertConfigurationIsValid();
-
-      var mapper = new Mapper(config);
-
-      var testResult = mapper.Map(md, ms);
-      */
-    }
-
-    [Test]
-    public void MergeTestAutomapper()
-    {
-      var md = new mTest { d = 5, e1 = eTest.NotSet, e2 = eTest.AVal };
-      var ms = new mTest { s = "Test", e1 = eTest.BVal };
-
-      eTest e;
-
-      var config = new MapperConfiguration(cfg =>
-      {
-        cfg.CreateMap<mTest, mTest>();
-        cfg.ForAllPropertyMaps(pm => true, (pm, c) => c.ResolveUsing(new IgnoreNullResolver(), pm.SourceMember.Name));
-      });
-
-      config.AssertConfigurationIsValid();
-
-      var m = config.CreateMapper();
-
-      var testResult = m.Map(md, ms);
-    }
-
-    [Test]
-    public void Merging()
-    {
-      //var config = new MapperConfiguration(cfg =>
-      //{
-        //cfg.CreateMap<List<double>, List<double>>().ConvertUsing<DoubleArrayTypeConverter>();
-        //cfg.CreateMap<double, double?>().ConvertUsing<ZeroDoubleNullTypeConverter>();
-        //cfg.CreateMap<double, double>().ForAllMembers()
-        //cfg.CreateMap<StructuralVectorSix, StructuralVectorSix>().ForAllMembers(dest => dest.PreCondition)
-
-        //cfg.CreateMap<double, double>().ForAllMembers(x => x.Condition((d, b) => d > 0));
-        //cfg.CreateMap<double, double>().ForAllMembers(opt => opt.PreCondition(src => (src > 0d)));
-        //cfg.CreateMap<double, double>().ConvertUsing<ZeroDoubleTypeConverter>();
-
-        //cfg.CreateMap<double, double>().BeforeMap((src, dest) => dest = -1);
-
-        //cfg.CreateMap<StructuralVectorSix, StructuralVectorSix>();
-        //cfg.ForAllPropertyMaps(pm => pm.SourceType == typeof(double) && pm.DestinationType == typeof(double), (pm, c) => c.PreCondition(o => (double) o > 0d));
-
-        //cfg.ForAllPropertyMaps(pm => pm.TypeMap.SourceType == typeof(double), (pm, c) => c.MapFrom(new IgnoreNullResolver(), pm.SourceMember.Name));
-
-        //cfg.CreateMap<StructuralSpringProperty, StructuralSpringProperty>().ForMember(dest => dest.ApplicationId, opt => opt.MapFrom(src => string.Join(" ", src.ApplicationId, "-Test")));
-        //cfg.CreateMap<StructuralSpringProperty, StructuralSpringProperty>().ForAllMembers(o => o.Condition((src, dest, value) => (double)value != 0d));
-        //cfg.CreateMap<StructuralSpringProperty, StructuralSpringProperty>().ForMember(d => d.DampingRatio, o => o.MapFrom<CustomerResolver>());
-      //}
-      //);
-    
-      /*
-      config.AssertConfigurationIsValid();
-      var mapper = new Mapper(config);
-
-      var vSrc = new StructuralVectorSix(new double[] { 0, 2, 3, 4, 5, 6 });
-      var vDest = new StructuralVectorSix(new double[] { 10, 20, 30, 40, 50, 60 });
-
-      var testResult = mapper.Map(vSrc, vDest);
-      
-      var ls1 = new object[] { "PROP_SPR.3:{speckle_app_id:gh/a}", 1, "LSPxGeneral", "NO_RGB", "GLOBAL", "GENERAL", 0, 16, 0, 17, 0, 18, 0, 19, 0, 20, 0, 21, 0.21 };
-      var gwa1 = string.Join("\t", ls1.Select(l => l.ToString()));
-
-      PrepareGwaToSpeckle<GSASpringProperty>(gwa1, "PROP_SPR.3", "gh/a");
-
-      Conversions.ToSpeckle(new GSASpringProperty());
-
-      var existing = ((IGSASpeckleContainer)Initialiser.GSASenderObjects[typeof(GSASpringProperty)].First()).Value;
-
-      var ls2 = new object[] { "PROP_SPR.3:{speckle_app_id:gh/a}", 1, "", "NO_RGB", "GLOBAL", "GENERAL", 0, 5, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0.5 };
-      var gwa2 = string.Join("\t", ls2.Select(l => l.ToString()));
-
-      PrepareGwaToSpeckle<GSASpringProperty>(gwa2, "PROP_SPR.3", "gh/a");
-
-      Conversions.ToSpeckle(new GSASpringProperty());
-      var newToMerge = ((IGSASpeckleContainer)Initialiser.GSASenderObjects[typeof(GSASpringProperty)].First()).Value;
-
-      var resultingObject = mapper.Map(newToMerge, existing);
-
-      */
-
-      /*
-        Mapper.Initialize(cfg =>
-        {
-          // necessary if you are mapping parent to a parent
-          cfg.CreateMap<Parent, Parent>()
-              .ForAllMembers(options =>
-              {
-                options.Condition(src => src.DestinationValue == null);
-              });
-          // necessary if you are mapping your child to a child
-          cfg.CreateMap<Child, Child>()
-              .ForAllMembers(options =>
-              {
-                options.Condition(src => src.DestinationValue == null);
-              });
-        });
-        */
-    }
-
-    private void PrepareGwaToSpeckle<T>(string gwaCommand, string keyword, string sid)
-    {
-      var testType = typeof(T);
-
-      var mockGsaInterfacer = new Mock<IGSAInterfacer>();
-      mockGsaInterfacer.Setup(x => x.GetGWARecords(It.IsAny<string>())).Returns((string x) =>
-      {
-        if (x.ToLower().StartsWith("get_all\tprop_spr"))
-        {
-          return new[] { gwaCommand };
-        }
-        return new[] { "" };
-      }
-        );
-      mockGsaInterfacer.Setup(x => x.GetSID(It.IsAny<string>(), It.IsAny<int>())).Returns((string x, int y) =>
-      {
-        var xLower = x.ToLower();
-        if (xLower.StartsWith("prop_spr"))
-        {
-          if (y == 1) return "gh/a";
-        }
-        return "";
-      }
-        );
-      mockGsaInterfacer.Setup(x => x.GetDeletedGWARecords(It.IsAny<string>())).Returns((string x) => { return new[] { "" }; });
-
-      Initialiser.GSASenderObjects = new Dictionary<Type, List<object>>
-      {
-        { testType, new List<object>() }
-      };
-      Initialiser.Interface = mockGsaInterfacer.Object;
-    }
-  }
-
-  /*
-  class IgnoreNullResolver : IMemberValueResolver<object, object, object, object>
-  {
-    public object Resolve(object source, object destination, object sourceMember, object destinationMember, ResolutionContext context)
-    {
-      return sourceMember ?? destinationMember;
-    }
-  }
-
-  public class CustomResolver : IValueResolver<object, object, double>
-  {
-    public double Resolve(object source, object dest, double destMember, ResolutionContext context)
-    {
-      return destMember * 2;
-    }
-  }
-
-  public class ZeroToNullIntTypeConverter : ITypeConverter<int, int?>
-  {
-    public int? Convert(int source, int? destination, ResolutionContext context)
-    {
-      if (source == 0)
-      {
-        return destination;
-      }
-      else
-      {
-        return source;
-      }
-    }
-  }
-
-  public class ZeroDoubleTypeConverter : ITypeConverter<double, double>
-  {
-    public double Convert(double source, double destination, ResolutionContext context)
-    {
-      if (source == 0)
-      {
-        return destination;
-      }
-      else
-      {
-        return source;
-      }
-    }
-  }
-
-  public class ZeroDoubleNullTypeConverter : ITypeConverter<double, double?>
-  {
-    public double? Convert(double source, double? destination, ResolutionContext context)
-    {
-      if (source == 0)
-      {
-        return null;
-      }
-      else
-      {
-        return source;
-      }
-    }
-  }
-  */
 }
